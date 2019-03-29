5790d136bf338038860eee3c322d78121f6c1b49
GuylianBollon <guylianbollon@hotmail.com>
2019-03-29 19:00:37 +0100

    Nieuwe commit

---
+++ Hegel_experiment/src/hegel_experiment/Hegel_experiment.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hegel_experiment;
+
+/**
+ *
+ * @author Hp
+ */
+public class Hegel_experiment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Object a = null;
+        Object b = null;
+        boolean c = nullifieer(a,b);
+        System.out.println(c);
+    }
+
+    private static boolean nullifieer(Object a, Object b) {
+        return a==b;
+    }
+    
+}
